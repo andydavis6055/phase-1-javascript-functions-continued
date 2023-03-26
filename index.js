5513a99c1e2c7dfe43c9fca7835d9a7fbf981bf9
Andy Davis <andydavis6055@gmail.com>
2023-03-25 19:16:12 -0600

    tests passing?

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`)
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(work = "go to the office") {
+    return (`This Monday, I will ${work}.`)
+}
+mondayWork("work from home ")
+
+function wrapAdjective(string = "*") {
+    return function(adjective = "a hard worker.") {
+      return `You are ${string}${adjective}${string}!`
+    };
+  }
